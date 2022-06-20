3a9f8356593c040b26cbf2f596dd2097c8448756
GerardoGarzon <ggonzalezg1300@alumno.ipn.mx>
2022-06-20 00:36:13 -0500

    Controller for existent devices

--- devices.py
+++ devices.py
@@ -1,5 +1,7 @@
+import threading
+
 from termcolor import colored, cprint
-from threading import Thread
+from agent import start_agent, generate_report
 from snmp_requests import *
 from database import DataBase
 import os
@@ -38,6 +40,10 @@ class Devices:
                     num_interfaces = snmp_get(devices[device]['community'], devices[device]['ip_address'],
                                               '1.3.6.1.2.1.2.1.0')
                     print_colored('No. interfaces', num_interfaces, 'green', ['bold'])
+                    interfaces_name = snmp_walk(devices[device]['community'], devices[device]['ip_address'],
+                                                '1.3.6.1.2.1.2.2.1.2')
+                    for name in interfaces_name:
+                        print_colored('\tInterface', name, 'green', ['bold'])
                 else:
                     print_colored('Estado', 'DOWN', 'red', ['bold'])
                     print_colored('No. interfaces', 'Desconocido', 'red', ['bold'])
@@ -48,23 +54,30 @@ class Devices:
         ip_address = input('Ingresa la dirección ip del dispositivo: ')
         snmp_version = input('Ingresa la version de SNMP configurada en el dispositivo: ')
         community = input('Ingresa la comunidad configurada en el dispositivo: ')
-        port = input('Ingresa el puerto configurado en el dispositivo (161 default): ')
+        port = input('Ingresa el puerto configurado en el dispositivo (161 default): ' or '161')
 
-        os.system('mkdir devices_files/' + ip_address)
+        os.system('mkdir data/devices_files/' + ip_address)
 
         self.devices_database.insert(host_name, ip_address, snmp_version, community, port)
+
+        new_agent = threading.Thread(target=start_agent, args=(host_name, ip_address, community, port))
+        new_agent.start()
 
         print(colored('\nDispositivo agregado exitosamente.', 'green'))
 
     def delete_devices(self):
         ip_address = input('Ingresa la dirección ip del dispositivo que desea eliminar: ')
         self.devices_database.delete(ip_address)
-        os.system('rm -r devices_files/' + ip_address)
+        os.system('rm -r data/devices_files/' + ip_address)
         print(colored('\nDispositivo eliminado exitosamente.', 'green'))
 
     def generate_report(self):
         ip_address = input('Ingresa la dirección ip del dispositivo que desea obtener su reporte: ')
-        pass
+        minutos = input('Ingrese el rango de minutos sobre el cual desea generar el reporte: ')
+        generate_report(self.devices_database.read(ip_address)["ip_address"],
+                        self.devices_database.read(ip_address)["community"],
+                        minutos,
+                        self.devices_database.read(ip_address)["host_name"])
 
 # number of interfaces: 1.3.6.1.2.1.2.1.0
 # interfaces name:      1.3.6.1.2.1.2.2.1.2.#
@@ -74,4 +87,3 @@ class Devices:
 # retransmitted:        1.3.6.1.2.1.6.12.0
 # retransmitted:        1.3.6.1.2.1.6.12.0
 # output datagrams:     1.3.6.1.2.1.4.10.0
-
