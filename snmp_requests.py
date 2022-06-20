2a928fb7851fa30f302dba8a101d8f0294db52a8
GerardoGarzon <ggonzalezg1300@alumno.ipn.mx>
2022-06-20 00:37:56 -0500

    SNMP walk added

--- snmp_requests.py
+++ snmp_requests.py
@@ -1,22 +1,51 @@
 from pysnmp.hlapi import *
+import datetime
 
 
 def snmp_get(community, host, oid):
-    result = None
-
+    result = ""
     errorIndication, errorStatus, errorIndex, varBinds = next(
         getCmd(SnmpEngine(),
                CommunityData(community),
-               UdpTransportTarget((host, 161), timeout=2.0, retries=0),
+               UdpTransportTarget((host, 161)),
                ContextData(),
                ObjectType(ObjectIdentity(oid))))
 
     if errorIndication:
-        return None
+        print(errorIndication)
     elif errorStatus:
-        return None
+        print('%s at %s' % (errorStatus.prettyPrint(), errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
     else:
         for varBind in varBinds:
             varB = (' = '.join([x.prettyPrint() for x in varBind]))
             result = varB.split()[2]
     return result
+
+
+def snmp_walk(community, host, oid):
+    result = []
+    for (errorIndication,
+         errorStatus,
+         errorIndex,
+         varBinds) in nextCmd(SnmpEngine(),
+                              CommunityData(community),
+                              UdpTransportTarget((host, 161)),
+                              ContextData(),
+                              ObjectType(ObjectIdentity(oid)),
+                              lexicographicMode=False):
+
+        if errorIndication:
+            print(errorIndication)
+            break
+        elif errorStatus:
+            print('%s at %s' % (errorStatus.prettyPrint(),
+                                errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
+            break
+        else:
+            for varBind in varBinds:
+                varB = (' = '.join([x.prettyPrint() for x in varBind]))
+                result.append(varB.split(' = ')[1])
+    return result
+
+print(snmp_walk('GerardoComunidad', '192.168.0.172', '1.3.6.1.2.1.1.1')[0].split(' ')[0])
+
